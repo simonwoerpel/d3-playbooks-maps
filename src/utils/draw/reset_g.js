c435fa54c5416bda364e7e3aa23230df1d7faf37
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-18 11:17:42 +0100

    Utils/draw: Fix duplicate `g` elements, add `--delete-on-update` class

--- src/utils/draw/reset_g.js
+++ src/utils/draw/reset_g.js
@@ -1,10 +1,12 @@
 // remove all children of the main `g` element
+// and all children of the base `element` that are flagged with a css class named `--delete-on-update`
 // prepare for re-drawing (e.g. during resize) with margins
 export default ({
   g,
+  element,
   margin
 }) => {
+  element.selectAll('.--delete-on-update').remove()
   g.selectAll('*').remove()
-  g.append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 }
