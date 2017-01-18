ea0235ce412404175bcd886c00cccdc1981f21ab
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-18 11:27:02 +0100

    Utils/data: Allow setting `geoData` directly

--- src/utils/geo/get_geo_data.js
+++ src/utils/geo/get_geo_data.js
@@ -4,18 +4,23 @@ export default ({
   isTopojson,
   topojsonLayerName,
   topojsonObjectsAccessor,
-  geoDataUrl
+  geoDataUrl,
+  geoData
 }) => {
-  return new Promise((resolve, reject) => {
-    // d3.json(geoDataUrl, (err, d) => err ? reject(err) : resolve(d))
-    d3.json(geoDataUrl, (err, d) => {
-      if (err) reject(err)
-      if (isTopojson) {
-        resolve(topojson.feature(d, d[topojsonObjectsAccessor][topojsonLayerName]))
-      } else {
-        resolve(d)
-      }
+  if (geoData) {
+    return new Promise(r => r(geoData))
+  } else {
+    return new Promise((resolve, reject) => {
+      // d3.json(geoDataUrl, (err, d) => err ? reject(err) : resolve(d))
+      d3.json(geoDataUrl, (err, d) => {
+        if (err) reject(err)
+        if (isTopojson) {
+          resolve(topojson.feature(d, d[topojsonObjectsAccessor][topojsonLayerName]))
+        } else {
+          resolve(d)
+        }
+      })
     })
-  })
+  }
 }
 
